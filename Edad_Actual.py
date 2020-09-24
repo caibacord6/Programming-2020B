4f4d8f366ed864b494e7f93723214ed1b84e7184
Alexander Paguatian <caibacord6@hotmail.com>
2020-09-24 09:14:48 -0500

    Carga calculo de Edad actual

---
+++ Edad_Actual.py
@@ -0,0 +1,13 @@
+"solicitar nombre y año de nacimiento "
+
+Fact = 2020 
+Nom = input('\n'"DIGITE SU NOMBRE: ")
+Fnac = input('\n'"DIGITE SU FECHA DE NACIMIENTO: ")
+rest = 0
+
+Fnac1 = int (Fnac)
+Fact1 = int (Fact)
+
+rest = Fact1 - Fnac1
+
+print('\n'"SU NOMBRE REGISTRADO ES: ",Nom,"Y SU EDAD ACTUAL ES: ",rest)
