633480a690341a004160fbf125654098507f1aca
Alexander Paguatian <caibacord6@hotmail.com>
2020-09-30 20:12:27 -0500

    El programa valida si es un numero par o si es impar

---
+++ Numero_Par.py
@@ -0,0 +1,16 @@
+'''
+Script que identifica si un numero ingresado por el usuario es PAR.
+'''
+N = 0
+
+print ("Ingrese un numero Entero: ")
+num = int(input())
+
+if num % 2 == 0 :
+    print ("El Numero es Par: ")
+    
+else:
+    print ("El Numero es Impar: ")
+
+
+
