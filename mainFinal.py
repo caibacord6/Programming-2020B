fc3bd1781573e8e606146744891f544c3ecdbc23
caibacord6 <70418255+caibacord6@users.noreply.github.com>
2020-12-17 17:51:09 -0500

    Add files via upload
    Main taller final

---
+++ mainFinal.py
@@ -0,0 +1,13 @@
+import os
+from logueo import *
+from categorias import *
+from proveedores import *
+from productos import *
+from clientes import *
+#Main
+os.system("clear")
+logueo()
+categorias()
+proveedores()
+productos()
+clientes()
